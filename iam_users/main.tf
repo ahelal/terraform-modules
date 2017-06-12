3352bb1b3b61902227226ebfe2c12a7c65f85893
Adham Helal <aa@hellofresh.com>
2017-06-12 16:11:32 +0200

    IAM policy user modules

---
+++ iam_users/main.tf
@@ -0,0 +1,6 @@
+resource "aws_iam_user" "iam_user" {
+  count         = "${length(var.usernames)}"
+  name          = "${element(var.usernames, count.index)}"
+  path          = "${lookup(var.paths, element(var.usernames, count.index), "/")}"
+  force_destroy = "${lookup(var.destroys, element(var.usernames, count.index), false )}"
+}
---
+++ iam_users/vars.tf
@@ -0,0 +1,47 @@
+variable "usernames" {
+  description = "A list of IAM Users"
+  type        = "list"
+}
+
+/* Defaults to "/" if not defined */
+variable "paths" {
+  description = "A map that defines AWS IAM userpaths "
+  type        = "map"
+  default     = {}
+}
+
+/* Defaults to false if not defined https://www.terraform.io/docs/providers/aws/r/iam_user.html#force_destroy*/
+variable "destroys" {
+  description = "A map of users force destroy options"
+  type        = "map"
+  default     = {}
+}
+
+/*
+## Vars boiler plate module vars
+variable "usernames" {
+  type = "list"
+}
+variable "paths" {
+  type = "map"
+}
+variable "destroys" {
+  type = "map"
+}
+*/
+
+
+/*
+## Example module
+usernames = ["neo", "trinity", "morpheus"]
+paths = {
+  neo     = "/matrix/clevel"
+  trinity = "/matrix/it"
+  # morpheus will default to "/" path since not defined
+}
+destroys = {
+  neo     =  true
+  # morpheus and trinity will default to false since not defined
+}
+*/
+
